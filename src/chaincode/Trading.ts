7042ee860db9d9fa47e35d97f8f039cc44a44652
Huruikagi <kbc14a04@stu.kawahara.ac.jp>
2017-12-15 18:56:28 +0900

    Tradingのチェーンコードを実装

--- src/chaincode/Trading.ts
+++ src/chaincode/Trading.ts
@@ -1,11 +1,16 @@
 import { ChaincodeMethodMapper } from './ChaincodeMethodMap';
 import { ChaincodeStub } from 'fabric-shim';
-import * as v from '../util/validate';
 import { assertArgs } from '../util/assertArgs';
-import { Chaincode } from "./Chaincode";
-import Shim = require("fabric-shim");
+import { Chaincode } from './Chaincode';
+import * as validate from '../util/validate';
+import Shim = require('fabric-shim');
+import {CouchQuery} from "../util/CouchQuery";
+import {getAllStateQueryResult} from "../util/getAllStateQueryResult";
 
 
+/**
+ * tradingチェーンコードに来たリクエストを各関数にマッピングします。
+ */
 const tradingMethods: ChaincodeMethodMapper = (methodName: string) => {
     switch (methodName) {
         case 'getTrading':
@@ -25,48 +30,151 @@ const tradingMethods: ChaincodeMethodMapper = (methodName: string) => {
 };
 
 
+/**
+ * 指定されたID値を持つ取引情報を取得します。
+ */
 async function getTrading(stub: ChaincodeStub, args: string[]): Promise<Buffer> {
-    assertArgs(args, [v.uuid]);
+    assertArgs(args, [validate.uuid]);
 
     const id = args[0];
-    return stub.getState(id);
+
+    const state = stub.getState(id);
+
+    if (state.toString()) {
+        throw new Error('not found trading id: ' + id);
+    }
+
+    return state;
 }
 
 
+/**
+ * 指定された条件に合致する取引情報のリストを取得します。
+ */
 async function getTradingsList(stub: ChaincodeStub, args: string[]): Promise<Buffer> {
+
+    const opt = validate.optional;
+
     assertArgs(args, [
-        v.optional(v.user.locator),
-        v.optional(v.user.locator),
-        v.optional(v.isbn13),
-        v.optional(v.boolean),
-        v.optional(v.uint),
-        v.optional(v.uint),
+        opt(validate.user.locator),
+        opt(validate.user.locator),
+        opt(validate.isbn13),
+        opt(validate.boolean),
+        opt(validate.uint),
+        opt(validate.uint),
     ]);
 
-    throw new Error('not implemented yet');
+    const owner = args[0];
+    const borrower = args[1];
+    const isbn = args[2];
+    const isReturned = args[3];
+    const limit = parseInt(args[4], 10);
+    const offset = parseInt(args[5], 10);
+
+    const query: CouchQuery = { selector: {} };
+
+    if (owner) {
+        query.selector.owner = owner;
+    }
+    if (borrower) {
+        query.selector.borrower = borrower;
+    }
+    if (isbn) {
+        query.selector.isbn = isbn;
+    }
+    if (isReturned) {
+        query.selector.$exists = 'returnedAt';
+    }
+    if (limit) {
+        query.limit = limit;
+    }
+    if (offset) {
+        query.skip = offset;
+    }
+
+    const iterator = await stub.getQueryResult(JSON.stringify(query));
+    const results = getAllStateQueryResult(iterator);
+
+    return Buffer.from(JSON.stringify(results));
 }
 
 
+/**
+ * 貸出情報を登録します。
+ */
 async function createTrading(stub: ChaincodeStub, args: string[]): Promise<Buffer> {
     assertArgs(args, [
-        v.user.locator,
-        v.user.locator,
-        v.isbn13,
+        validate.uuid,
+        validate.user.locator,
+        validate.user.locator,
+        validate.isbn13,
+        validate.datetime,
     ]);
 
-    throw new Error('not implemented yet');
+    const id = args[0];
+
+    const exist = await stub.getState(id);
+
+    if (exist.toString()) {
+        // 既になんか存在していたとき
+        throw new Error('duplicate trading id: ' + id);
+    }
+
+    const owner = args[0];
+    const borrower = args[1];
+    const isbn = args[2];
+    const lendAt = args[3];
+
+    const trading = {
+        id,
+        owner,
+        borrower,
+        isbn,
+        lendAt,
+    };
+
+    return Buffer.from(JSON.stringify(trading));
+
 }
 
 
+/**
+ * 指定された貸出情報を返却済みとして更新します。
+ */
 async function markTradingReturned(stub: ChaincodeStub, args: string[]): Promise<Buffer> {
-    assertArgs(args, [v.uuid]);
+    assertArgs(args, [
+        validate.uuid,
+        validate.datetime,
+    ]);
 
-    throw new Error('not implemented yet');
+    const id = args[0];
+    const lendAt = args[1];
+
+    const currentBuffer = await stub.getState(id);
+
+    if (!currentBuffer.toString()) {
+        // 更新しようとしたのになかったらエラー
+        throw new Error('not found trading id: ' + id);
+    }
+
+    const current = JSON.parse(Buffer.toString());
+    const updated = {
+        ...current,
+        lendAt,
+    };
+
+    const updatedBuffer = Buffer.from(JSON.stringify(updated));
+    await stub.putState(id, updatedBuffer);
+
+    return updatedBuffer;
 }
 
 
+/**
+ * TODO: どうしよう。
+ */
 async function cancelTrading(stub: ChaincodeStub, args: string[]): Promise<Buffer> {
-    assertArgs(args, [v.uuid]);
+    assertArgs(args, [validate.uuid]);
 
     throw new Error('not implemented yet');
 }
